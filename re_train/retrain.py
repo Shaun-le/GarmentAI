d8d411b32e46aa1f8b1fea69ec980301e28908d8
Shaun <lehuuloi.cs@gmail.com>
2023-08-02 23:53:01 +0700

    update re-train

---
+++ re_train/retrain.py
@@ -0,0 +1,54 @@
+# Import libraries
+from flask import Flask, request, jsonify
+from re_train.utils import train_with_Kfolds, create_dummy_variables, read_data, trans_output
+import shutil
+
+app = Flask(__name__)
+@app.route('/rtr', methods = ['POST'])
+def main():
+    data = request.get_json(force=True)
+    product = data.get('Product')
+    task = data.get('Task')
+    df = read_data(data.get('Data'))
+    result = {}
+    if product == 'TSPS':
+        if task == 'NS':
+            X = df.drop(columns=['Y']).values
+            y = df['Y'].values
+            loss = train_with_Kfolds(X, y, 'models/NS_TSPS.pkl', True, False)
+            result = trans_output(loss, y, True)
+        elif task == 'DMC':
+            X = df.drop(columns=['Y1', 'Y2', 'Y3', 'Y4']).values
+            y = df['Y1'].values
+            loss = train_with_Kfolds(X, y, 'models/DMC_TSPS.pkl', True, False)
+            result = trans_output(loss, y, True)
+        elif task == 'QTCN':
+            df = create_dummy_variables(df, 'Y')
+            X = df.iloc[:,:9].values
+            y = df.iloc[:,9:].values
+            score = train_with_Kfolds(X, y, 'models/QTCN_TSPS.pkl', False, True)
+            result = trans_output(score, y, False)
+    elif product == "SM":
+        if task == "QTCN":
+            print(df)
+    elif product == 'Jacket':
+        if task == 'QTCN':
+            df = create_dummy_variables(df, 'Y')
+            X = df.iloc[:,:15].values
+            y = df.iloc[:,15:].values
+            score = train_with_Kfolds(X, y, 'models/QTCN_J.pkl', False, True)
+            result = trans_output(score, y, False)
+    return result
+
+@app.route('/dl', methods = ['POST'])
+def deploy():
+    data = request.get_json(force=True)
+    product = data.get('Product')
+    task = data.get('Task')
+    if product == "Jacket":
+        if task == "QTCN":
+            shutil.move('models/QTCN_J.pkl', 'D:/May/GarmentAI/model')
+    return jsonify({"prediction": f"Mô Hình Đã Cập Nhật"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ re_train/utils.py
@@ -0,0 +1,109 @@
+from flask import jsonify
+from sklearn.ensemble import AdaBoostRegressor, AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error, accuracy_score
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import KFold, train_test_split
+import pickle
+from sklearn.metrics import f1_score
+import pandas as pd
+import re
+from prepare_data import fh
+
+def read_data(data):
+    df = pd.DataFrame(eval(data)).transpose()
+    df.columns = ['X' + str(i) for i in range(1, len(df.columns) + 1)]
+    df.rename(columns={df.columns[-1]: 'Y'}, inplace=True)
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].str.strip()
+            df.iloc[:,:-1] = fh.transform(df.iloc[:,:-1])
+    return df
+
+def trans_output(score, y, is_regression = True):
+    if is_regression:
+        return jsonify({"prediction": f"Hoàn thành: Loss = {score:.2f} (Tỷ lệ sai số: {score / y.mean() * 100:.2f}%)"})
+    return jsonify({"prediction": f"Hoàn thành: Độ chính xác = {score:.2f}%"})
+
+def create_dummy_variables(df, column_name):
+    values = set()
+    for value in df[column_name]:
+        values.update(value.split('; '))
+
+    for value in values:
+        pattern = re.compile(r'\b{}\b'.format(re.escape(value)))
+        df[value] = df[column_name].apply(lambda x: 1 if re.search(pattern, x) else 0)
+
+    df.drop(column_name, axis=1, inplace=True)
+    return df
+
+def train_with_Kfolds(X, y, path, is_regression=True, is_multi=True):
+    X_train, X_test_g, y_train, y_test_g = train_test_split(X, y, test_size=0.1)
+
+    if is_regression:
+        if is_multi:
+            model = MultiOutputRegressor(AdaBoostRegressor())
+        else:
+            model = AdaBoostRegressor()
+    else:
+        if is_multi:
+            model = MultiOutputClassifier(AdaBoostClassifier())
+        else:
+            model = AdaBoostClassifier()
+
+    num_folds = 5
+    kf = KFold(n_splits=num_folds, shuffle=True, random_state=0)
+
+    for train_index, test_index in kf.split(X_train):
+        X_train_fold, X_test_fold = X[train_index], X[test_index]
+        y_train_fold, y_test_fold = y[train_index], y[test_index]
+
+        model.fit(X_train_fold, y_train_fold)
+
+    if is_regression:
+        y_pred = model.predict(X_test_g)
+        score = mean_absolute_error(y_test_g, y_pred)
+        if (score / y_test_g.mean() * 100) < 2:
+            with open(path, 'wb') as file:
+                pickle.dump(model, file)
+            return score
+    else:
+        y_pred = model.predict(X_test_g)
+        score = accuracy_score(y_pred.flatten(),y_test_g.flatten())
+        #if score > 0.7:
+        with open(path, 'wb') as file:
+            pickle.dump(model, file)
+        return score
+
+    return train_with_Kfolds(X, y, path, is_regression, is_multi)
+
+'''def train_with_Kfolds(X, y, path):
+    X_train, X_test_g, y_train, y_test_g = train_test_split(X, y, test_size=0.1)
+
+    # Define the model and its hyperparameter grid
+    model = AdaBoostRegressor()
+    param_grid = {
+        'n_estimators': [50, 100, 150],
+        'learning_rate': [0.01, 0.1, 0.5],
+    }
+
+    num_folds = 5
+    kf = KFold(n_splits=num_folds, shuffle=True, random_state=0)
+
+    # Perform grid search with k-fold cross-validation
+    grid_search = GridSearchCV(model, param_grid, cv=kf, scoring='neg_mean_squared_error')
+    grid_search.fit(X_train, y_train)
+
+    best_model = grid_search.best_estimator_
+
+    # Evaluate the model on the hold-out test set
+    score = eval_regression(best_model, X_test_g, y_test_g)
+
+    if (score / y.mean() * 100) < 3:
+        with open(path, 'wb') as file:
+            pickle.dump(best_model, file)
+        return f"{score / y.mean() * 100:.2f}"
+    else:
+        # If the performance is not satisfactory, try again with K-fold cross-validation
+        return train_with_Kfolds(X, y, path)'''
